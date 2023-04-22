3eb1590e41ae798a192cd8628f0cb6b2969bcb7c
Fernan <lfortega0625@gmail.com>
2023-04-21 20:15:05 -0400

    Updates

---
+++ precache-manifest.36e5c68d69af56f9c38eba1627b6f1d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd111e3333802139ffa4",
+    "url": "/portfolio/static/css/main.5603ee4c.chunk.css"
+  },
+  {
+    "revision": "fd111e3333802139ffa4",
+    "url": "/portfolio/static/js/main.15735cbc.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "ce40530c148235ea42e1",
+    "url": "/portfolio/static/js/2.924849ce.chunk.js"
+  },
+  {
+    "revision": "a393772962b85ad24c3e8fa746ee64cf",
+    "url": "/portfolio/index.html"
+  }
+];
